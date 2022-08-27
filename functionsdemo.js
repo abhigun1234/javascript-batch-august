7da6e3a519451d3e0c6215974f08e6fae56600e9
Abhishek <abhi.ghosh27feb@gmail.com>
2022-08-27 17:13:06 +0530

    add

--- functionsdemo.js
+++ functionsdemo.js
@@ -6,16 +6,25 @@
 //multiplication
 //division
 
- let no1=prompt("enter first no")
+//  let no1=prompt("enter first no")
 
-let no2=prompt("enter second no")
- var result =mul(no1,no2)
- console.log("result",result)
- alert(result)
+// let no2=prompt("enter second no")
+//  var result =mul(no1,no2)
+//  console.log("result",result)
+//  alert(result)
 //multiplication
 
-function mul(no1,no2){
-
-    return no1*no2
+function mul(){
+    var number1=document.getElementById("number1").value;  
+    var number2=document.getElementById("number2").value;  
+    var result=number1*number2
+    document.getElementById("result").innerHTML = result;
+   
 }
- 
+function add(){
+    var number1=document.getElementById("number1").value;  
+    var number2=document.getElementById("number2").value;  
+    var result=Number(number1)+Number(number2)
+    document.getElementById("result").innerHTML = result;
+   
+}
