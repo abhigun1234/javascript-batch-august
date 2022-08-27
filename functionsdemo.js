e17edfb252a949a9d393f5d53056a67592ba93b7
Abhishek <abhi.ghosh27feb@gmail.com>
2022-08-27 16:47:18 +0530

    added calc

--- functionsdemo.js
+++ functionsdemo.js
@@ -1,6 +1,21 @@
 // defination
-function hello(str){
+// calc
 
-    console.log("hello",str)
+// addition 
+// substraction
+//multiplication
+//division
+
+ let no1=prompt("enter first no")
+
+let no2=prompt("enter second no")
+ var result =mul(no1,no2)
+ console.log("result",result)
+ alert(result)
+//multiplication
+
+function mul(no1,no2){
+
+    return no1*no2
 }
-hello("abhishek")
+ 
