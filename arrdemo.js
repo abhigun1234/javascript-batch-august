e03e6bfbcfa4e517a08f225efb031fa09c84453e
Abhishek <abhi.ghosh27feb@gmail.com>
2022-09-04 17:12:30 +0530

    arrays methods

--- arrdemo.js
+++ arrdemo.js
@@ -28,16 +28,16 @@
 // 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-fruits.push("sweet lime")// push adding eliment end 
-console.log("fruits",fruits)
-fruits.unshift("Sweet Lime")
-console.log("fruits",fruits)
-fruits.pop()
-console.log("fruits",fruits)
-fruits.shift()
-console.log("fruits",fruits)
-// fruits.splice( 3); 
+// fruits.push("sweet lime")// push adding eliment end 
 // console.log("fruits",fruits)
+// fruits.unshift("Sweet Lime")
+// console.log("fruits",fruits)
+// fruits.pop()
+// console.log("fruits",fruits)
+// fruits.shift()
+// console.log("fruits",fruits)
+fruits.splice( 0,3); 
+console.log("fruits",fruits)
 // delete fruits; 
 // console.log("fruits",fruits)
 
